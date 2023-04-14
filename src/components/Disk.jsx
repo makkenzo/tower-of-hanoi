ea96e060ab3048432db3880d04634a7c47c27771
makkenzo <nekgo2009@gmail.com>
2023-04-14 20:13:51 +0600

    Updates

--- src/components/Disk.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Disk = ({ size }) => {
-    const width = size * 12 + 40;
-    return <div className={`bg-[#E94F37] mb-2 rounded-lg`} style={{ width: `${width}px`, height: '20px' }}></div>;
-};
-
-export default Disk;
